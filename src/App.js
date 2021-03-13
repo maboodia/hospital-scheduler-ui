4d71238575d916fdc51ae48263a572c7a5c70adb
Muhamad Aboodia <muhamad.aboodia@outlook.com>
2021-03-13 05:33:31 +0000

    First UI version.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [patientsData, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:8080/v1/demo/hospital-scheduling/patients',
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <th>Patient Name</th>
+        <th>Schedule</th>
+      </thead>
+      <tbody>
+        {patientsData.map(item => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.schedules[0].startDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
