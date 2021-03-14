d34d6fef758aea5905a1b83a021879996e5626b8
Muhamad Aboodia <muhamad.aboodia@outlook.com>
2021-03-14 03:36:53 +0100

    Add schedule view added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "react-datetime/css/react-datetime.css";
+import Datetime from "react-datetime";
 
 function App() {
   const [patientsData, setData] = useState([]);
@@ -18,7 +20,31 @@ function App() {
 
   return (
     <div>
+
+      <h1>Add New Schedule</h1>
+
+      <label for="fname">Pateint:</label>
+      <br/>
+      <select>
+        <option value="" selected disabled hidden></option>
+        {patientsData.map(item => (
+          <option value={item.id}>{item.id} - {item.name}</option>
+        ))}
+      </select>
+      <br/><br/>
+
+      <label for="lname">Date:</label>
+      <br/>
+      <Datetime
+        timeConstraints={{ hours: { min: 0, max: 23 },
+                         minutes: {step:10} }}
+      />
+      <br/><br/>
+
+      <input type="submit" value="Submit"/>
+
       <h1>Schdules List</h1>
+
       {patientsData.map(item => {
         return (item.schedules.length > 0) ?
         <ul>
