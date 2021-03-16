4b40186f13ade2134bb5ab393bd4c93474e33c16
Muhamad Aboodia <muhamad.aboodia@outlook.com>
2021-03-16 05:46:44 +0100

    mock axios for page refresh test.

--- src/test/unit-tests/App.test.js
+++ src/test/unit-tests/App.test.js
@@ -1,5 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from '../../App';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const patientsData = require('../test-data/patients-data');
 
 test('renders the application', () => {
   render(<App />);
@@ -12,5 +17,23 @@ test('renders the application', () => {
 
   const refreshButton = screen.getByText(/Refresh/i);
   expect(refreshButton).toBeInTheDocument();
+
+});
+
+test('click the refresh button', async () => {
+
+  var mock = new MockAdapter(axios);
+  mock.onGet('http://localhost:8080/v1/demo/hospital-scheduling/patients').reply(200, patientsData);
+
+
+  const { getByText } = render(<App />);
+
+  const refreshButton = screen.getByText(/Refresh/i);
+  fireEvent.click(refreshButton);
+
+  await waitFor(() => {
+    const patientNameList = screen.getAllByText(/Muhamad Aboodia/i);
+    expect(patientNameList).toHaveLength(2);
+  });
 
 });
